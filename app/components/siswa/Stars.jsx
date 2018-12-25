bb83c7ea73a3c49d31648fb19c402b4c7fb499db
Rahadyan Fannani Arif <rahadyan.xc@gmail.com>
2018-12-26 03:49:32 +0700

    stars rules

--- app/components/siswa/Stars.jsx
+++ app/components/siswa/Stars.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import star from "../../assets/images/star.png";
+import starOff from "../../assets/images/star-off.png";
 import styled from "styled-components";
+import classnames from "classnames";
 
 const Star = styled.img`
-  margin-left:5px;
+  
+`
+
+const Desc = styled.p`
+  font-size:14px;
 `
 
 const Stars = ({className,value}) => {
-  let starImg = [];
-  for (let i = 0; i < value; i++) {
-    starImg.push(<Star width="50px" key={i} src={star} />);
-  }
-
   return (
-    <div className={className}>
-      {starImg}
+    <div className={classnames(className,'row justify-content-center')}>
+        <div className="col-3">
+          <Star width="50px"  src={(value[0]) ? star : starOff} />
+          <Desc>Selesaikan Stage</Desc>
+        </div>
+        <div className="col-3">
+          <Star width="50px"  src={(value[1]) ? star : starOff} />
+        <Desc>Selesaikan dalam 3 menit</Desc>
+        </div>
+        <div className="col-3">
+          <Star width="50px"  src={(value[2]) ? star : starOff} />
+          <Desc>Nyawa lebih dari 1</Desc>
+        </div>
+
     </div>
   )
 }
