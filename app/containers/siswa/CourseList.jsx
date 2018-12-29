791bdbdcca818333e385daef9308054a84666966
Rahadyan Fannani Arif <rahadyan.xc@gmail.com>
2018-12-30 05:31:26 +0700

    loader

--- app/containers/siswa/CourseList.jsx
+++ app/containers/siswa/CourseList.jsx
@@ -3,15 +3,55 @@ import React, { Component } from 'react';
 import CourseItem from '../../components/siswa/CourseItem';
 import { Query } from 'react-apollo';
 import { GET_COURSES } from '../../graphql/coursesQuery';
+import ContentLoader, { Facebook } from 'react-content-loader';
+import { BASE_URL } from '../../config/config';
 
+const Loader = () => {
+  const content = (
+    <div
+      className="card m-2"
+      style={{
+        width: '200px',
+        height: '250px',
+        borderRadius: '10px !important',
+        border: '0',
+      }}
+    >
+      <ContentLoader width={200} height={250}>
+        <circle cx="100" cy="80" r="60" />
+        <rect x="20" y="160" rx="4" ry="4" width="160" height="15" />
+        <rect x="20" y="185" rx="4" ry="4" width="160" height="15" />
+        <rect x="20" y="210" rx="4" ry="4" width="130" height="15" />
+      </ContentLoader>
+    </div>
+  );
+  let all = [];
+  for (let i = 0; i < 6; i++) {
+    all.push(content);
+  }
+  return (
+    <div>
+      <div className="d-flex flex-wrap">{all}</div>
+    </div>
+  );
+};
 class CourseList extends Component {
+  constructor(props) {
+    super(props);
+    // this.state = {
+    //   show: false
+    // }
+    // setTimeout(function() {
+    //   this.setState({show: true});
+    // }.bind(this),700)
+  }
   render() {
     return (
       <div>
-        <h2 style={{marginLeft : '30px',fontSize:'40px'}}>All Course</h2>
+        <h2 style={{ marginLeft: '30px', fontSize: '40px' }}>All Course</h2>
         <Query query={GET_COURSES}>
           {({ loading, error, data }) => {
-            if (loading) return <p>Loading…</p>;
+            if (loading) return <Loader />
             if (error)
               return <p>Sorry! There was an error loading the items</p>;
             return (
@@ -27,4 +67,4 @@ class CourseList extends Component {
     );
   }
 }
-export default CourseList
+export default CourseList;
