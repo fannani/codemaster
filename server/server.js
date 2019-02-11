6344802ab664fa56debf0e5ac2a1b704d0d6fc70
Rahadyan Fannani Arif <rahadyan.xc@gmail.com>
2019-02-11 20:39:36 +0700

    tes

--- server/server.js
+++ server/server.js
@@ -12,8 +12,7 @@ import schema from './data/schema';
 import './config/passport';
 import { ApolloServer } from 'apollo-server-express';
 import dotenv from 'dotenv';
-
-dotenv.config();
+dotenv.config({path: path.join(__dirname, "../.env")});
 const { ObjectId } = mongoose.Types;
 var compiler = webpack(webpackConfig);
 let port = process.env.PORT;
