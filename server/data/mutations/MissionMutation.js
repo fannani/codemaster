50493cf27623c0bce21d2d938d4a1384fd4ca415
Rahadyan Fannani Arif <rahadyan.xc@gmail.com>
2019-03-16 23:30:15 +0700

    update mission mutation

--- server/data/mutations/MissionMutation.js
+++ server/data/mutations/MissionMutation.js
@@ -6,8 +6,11 @@ import {
   GraphQLList,
 } from 'graphql';
 
+import { GraphQLUpload } from 'graphql-upload/lib';
 import MissionType from '../types/MissionType';
 import Mission from '../models/Mission';
+import StageType from '../types/StageType';
+import Stage from '../models/Stage';
 
 const MissionMutation = {
   addMission: {
@@ -33,6 +36,23 @@ const MissionMutation = {
           err ? reject(err) : resolve(newmission);
         });
       }),
+  },
+  updateMission: {
+    type: MissionType,
+    description: 'Update Mission',
+    args: {
+      id: { type: GraphQLNonNull(GraphQLID) },
+      quest: { type: GraphQLNonNull(GraphQLString) },
+      testcase: { type: GraphQLList(GraphQLID) },
+      params: { type: GraphQLList(GraphQLString) },
+      score: { type: GraphQLNonNull(GraphQLInt) },
+      stage: { type: GraphQLNonNull(GraphQLID) },
+    },
+    async resolve(root, args) {
+      const editmission = await Stage.findById(args.id);
+      editmission.set(args);
+      return await editmission.save();
+    },
   },
 };
 
