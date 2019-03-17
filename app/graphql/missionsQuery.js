471522a5f43a621af695b3b96d247f5b0eebd46b
Rahadyan Fannani Arif <rahadyan.xc@gmail.com>
2019-03-17 14:02:18 +0700

    ADD_TESTCASE_MISSION query

--- app/graphql/missionsQuery.js
+++ app/graphql/missionsQuery.js
@@ -6,46 +6,36 @@ export const GET_MISSION_BY_ID = gql`
       _id
       quest
       score
-      testcase
-      params
     }
   }
 `;
 
 export const ADD_MISSION = gql`
-  mutation addMission(
-    $quest: String!
-    $testcase: [String]
-    $score: Int!
-    $stageid: ID!
-  ) {
-    addMission(
-      quest: $quest
-      testcase: $testcase
-      score: $score
-      stage: $stageid
-    ) {
+  mutation addMission($quest: String!, $score: Int!, $stageid: ID!) {
+    addMission(quest: $quest, score: $score, stage: $stageid) {
       _id
     }
   }
 `;
 
 export const UPDATE_MISSION = gql`
-  mutation updateMission(
-    $id: ID!
-    $quest: String
-    $testcase: [ID]
+  mutation updateMission($id: ID!, $quest: String, $score: Int, $stage: ID) {
+    updateMission(id: $id, quest: $quest, score: $score, stage: $stage) {
+      _id
+    }
+  }
+`;
+
+export const ADD_TESTCASE_MISSION = gql`
+  mutation addTestCaseMission(
+    $mission: ID!
+    $testcase: ID!
     $params: [String]
-    $score: Int
-    $stage: ID
   ) {
-    updateMission(
-      id: $id
-      quest: $quest
+    addTestCaseMission(
+      mission: $mission
       testcase: $testcase
       params: $params
-      score: $score
-      stage: $stage
     ) {
       _id
     }
