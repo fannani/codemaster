45d493cf19db0bb3411701bc2143e4fe500479ab
Rahadyan Fannani Arif <rahadyan.xc@gmail.com>
2019-03-17 14:33:02 +0700

    fixing testcase mission query

--- server/data/types/TestCaseMissionType.js
+++ server/data/types/TestCaseMissionType.js
@@ -5,6 +5,9 @@ import {
   GraphQLID,
   GraphQLList,
 } from 'graphql';
+import TestCaseType from './TestCaseType';
+import TestCaseMission from '../models/TestCaseMission';
+import TestCase from '../models/TestCase';
 
 const TestCaseMissionType = new GraphQLObjectType({
   name: 'TestCaseMission',
@@ -12,7 +15,13 @@ const TestCaseMissionType = new GraphQLObjectType({
   fields: () => ({
     _id: { type: GraphQLNonNull(GraphQLID) },
     mission: { type: GraphQLNonNull(GraphQLID) },
-    testcase: { type: GraphQLNonNull(GraphQLID) },
+    testcase: {
+      type: TestCaseType,
+      async resolve({ _id }) {
+        const data = await TestCaseMission.findOne({ _id });
+        return await TestCase.findOne({ _id: data.testcase });
+      },
+    },
     params: { type: GraphQLList(GraphQLString) },
     updated_at: { type: GraphQLNonNull(GraphQLString) },
   }),
