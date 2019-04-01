176d80d7c135855c42132cc2e2fb51ccf7ee1900
Rahadyan Fannani Arif <rahadyan.xc@gmail.com>
2019-04-02 00:43:53 +0700

    fixing admin constant

--- app/hooks/admin.js
+++ app/hooks/admin.js
@@ -1,14 +1,15 @@
 import { useContext } from 'react';
 import AppContext from '../utils/context';
 import UserService from '../services/UserService';
+import {LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT_SUCCESS, LOGOUT_REQUEST} from '../data/admin/types';
 
 const useAdmin = () => {
   const [state, dispatch] = useContext(AppContext);
 
   const login = (email, password) => {
-    const request = () => ({ type: 'LOGIN_REQUEST' });
+    const request = () => ({ type: LOGIN_REQUEST });
     const success = (user, isLogin) => ({
-      type: 'LOGIN_SUCCESS',
+      type: LOGIN_SUCCESS,
       user,
       isLogin,
     });
@@ -25,8 +26,8 @@ const useAdmin = () => {
   };
 
   const logout = () => {
-    const request = () => ({ type: 'LOGOUT_REQUEST' });
-    const success = () => ({ type: 'LOGOUT_SUCCESS' });
+    const request = () => ({ type: LOGOUT_REQUEST });
+    const success = () => ({ type: LOGOUT_SUCCESS });
     dispatch(request());
     UserService.logout();
     dispatch(success());
