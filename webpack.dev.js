03c4ea8ae3663a39857eb5b9f6f37b1e3eeb7169
Rahadyan Fannani Arif <rahadyan.xc@gmail.com>
2019-04-09 15:59:54 +0700

    test

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,8 +6,5 @@ module.exports = merge.smart(common, {
   entry: ['./app/app.jsx', 'webpack-hot-middleware/client'],
   mode: 'development',
   devtool: 'inline-source-map',
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ]
-
+  plugins: [new webpack.HotModuleReplacementPlugin()],
 });
