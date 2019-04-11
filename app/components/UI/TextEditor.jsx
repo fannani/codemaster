8a0d02bddabad4a23cef0928c0f22a8afda1db18
Rahadyan Fannani Arif <rahadyan.xc@gmail.com>
2019-04-12 02:52:42 +0700

    fixing code editor

--- app/components/UI/TextEditor.jsx
+++ app/components/UI/TextEditor.jsx
@@ -1,26 +1,41 @@
 import React, { useState, useCallback, useEffect } from 'react';
-import { EditorState, RichUtils } from 'draft-js';
+import { EditorState, RichUtils, Modifier } from 'draft-js';
 import Editor, { composeDecorators } from 'draft-js-plugins-editor';
 import createImagePlugin from 'draft-js-image-plugin';
 import createFocusPlugin from 'draft-js-focus-plugin';
 import createResizeablePlugin from 'draft-js-resizeable-plugin';
+import createBlockDndPlugin from 'draft-js-drag-n-drop-plugin';
 import firebase from 'firebase/app';
 import 'firebase/storage';
 import shortid from 'shortid';
 import { useDropzone } from 'react-dropzone';
 import 'draft-js-focus-plugin/lib/plugin.css';
 import Modal from 'react-bootstrap4-modal';
+import Prism from 'prismjs';
+import createPrismPlugin from 'draft-js-prism-plugin';
+import 'prismjs/themes/prism.css';
 
 const focusPlugin = createFocusPlugin();
 const resizeablePlugin = createResizeablePlugin();
+const blockDndPlugin = createBlockDndPlugin();
+const prismPlugin = createPrismPlugin({
+  prism: Prism,
+});
 
 const decorator = composeDecorators(
   focusPlugin.decorator,
   resizeablePlugin.decorator,
+  blockDndPlugin.decorator,
 );
 const imagePlugin = createImagePlugin({ decorator });
 
-const plugins = [focusPlugin, resizeablePlugin, imagePlugin];
+const plugins = [
+  focusPlugin,
+  resizeablePlugin,
+  imagePlugin,
+  blockDndPlugin,
+  prismPlugin,
+];
 
 const storageRef = firebase.storage().ref();
 let childRef;
@@ -39,7 +54,6 @@ const TextEditor = () => {
     });
   }, []);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
-
   useEffect(
     () => {
       if (loadingImage) {
@@ -63,6 +77,45 @@ const TextEditor = () => {
   const onImageClick = () => {
     setShowModal(true);
   };
+  const styleMap = {
+    CODE: {
+      backgroundColor: 'rgba(0, 0, 0, 0.05)',
+      fontFamily: '"Inconsolata", "Menlo", "Consolas", monospace',
+      fontSize: 16,
+      marginBottom: '-10px',
+      padding: 2,
+    },
+  };
+  const onScriptClick = () => {
+    // const selection = editorState.getSelection();
+    // let currentContent = editorState.getCurrentContent();
+    // if (
+    //   currentContent.getBlockForKey(selection.getStartKey()).getKey() !==
+    //   'code-block'
+    // ) {
+    //   currentContent = Modifier.setBlockType(
+    //     currentContent,
+    //     selection,
+    //     'code-block',
+    //   );
+    //
+    //   const blockMap = currentContent.getBlockMap();
+    //   const block = currentContent.getBlockForKey(selection.getStartKey());
+    //
+    //   const data = block.getData().merge({ language: 'javascript' });
+    //   const newBlock = block.merge({ data });
+    //   const newContentState = currentContent.merge({
+    //     blockMap: blockMap.set(block.getKey(), newBlock),
+    //     selectionAfter: selection,
+    //   });
+    //
+    //   setEditorState(
+    //     EditorState.push(editorState, newContentState, 'change-block-data'),
+    //   );
+    // } else {
+    onChange(RichUtils.toggleBlockType(editorState, 'code-block'));
+    // }
+  };
 
   const handleKeyCommand = command => {
     const newState = RichUtils.handleKeyCommand(editorState, command);
@@ -72,7 +125,6 @@ const TextEditor = () => {
     }
     return 'not-handled';
   };
-
   return (
     <>
       <div>
@@ -82,9 +134,13 @@ const TextEditor = () => {
         <button type="button" className="bold" onClick={onImageClick}>
           Image
         </button>
+        <button type="button" className="bold" onClick={onScriptClick}>
+          Script
+        </button>
       </div>
       <Editor
         editorState={editorState}
+        customStyleMap={styleMap}
         handleKeyCommand={handleKeyCommand}
         onChange={onChange}
         plugins={plugins}
